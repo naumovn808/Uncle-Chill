3bb64fd5fde07e245376e7ecedd33bf7a60ecc0c
aaa <aaa>
2024-07-28 19:00:22 +0500

    feat: add manifest, and add setting for gulp

--- gulp/compileStyles.mjs
+++ gulp/compileStyles.mjs
@@ -1,22 +1,29 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
-import dartSass from 'sass';
 import cleanCSS from 'gulp-clean-css';
 import sourcemaps from 'gulp-sourcemaps';
+import rename from 'gulp-rename';
+import dartSass from 'sass';
 
-sass.compiler = dartSass;
+const compileSass = sass(dartSass);
 
-const compileStyles = () =>
-    gulp.src('source/sass/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('build/css'));
+const compileStyles = () => {
+  return gulp.src('source/sass/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(compileSass().on('error', compileSass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'));
+};
 
-const compileMinStyles = () =>
-    gulp.src('source/sass/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('build/css'));
+const compileMinStyles = () => {
+  return gulp.src('source/sass/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(compileSass().on('error', compileSass.logError))
+    .pipe(cleanCSS())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/css'));
+};
 
 export { compileStyles, compileMinStyles };
--- gulp/copyAssets.mjs
+++ gulp/copyAssets.mjs
@@ -1,8 +1,5 @@
 import gulp from 'gulp';
 
-const copy = () =>
-    gulp.src('source/**/*.{html,php,json,webp}')
-        .pipe(gulp.dest('build'));
 
 const copyImages = () =>
     gulp.src('source/images/**/*.{png,jpg,jpeg,svg}')
@@ -11,5 +8,17 @@ const copyImages = () =>
 const copySvg = () =>
     gulp.src('source/images/**/*.svg')
         .pipe(gulp.dest('build/images'));
+
+const copy = () =>
+  gulp.src([
+    'source/**.html',
+    'source/fonts/**',
+    'source/img/**',
+    'source/favicon.ico',
+    'source/manifest.json'
+  ], {
+    base: 'source',
+  })
+    .pipe(gulp.dest('build'));
 
 export { copy, copyImages, copySvg };
