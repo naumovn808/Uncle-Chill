5f7d9e2256afd6af5ade97119665be9c24c07df2
aaa <aaa>
2024-07-31 15:10:06 +0500

    fix: fix error gasp

--- gulp/compileScripts.mjs
+++ gulp/compileScripts.mjs
@@ -1,33 +1,34 @@
 import gulp from 'gulp';
 import sourcemaps from 'gulp-sourcemaps';
-import concat from 'gulp-concat';
 import browserify from 'browserify';
 import uglify from 'gulp-uglify';
 import vinylBuffer from 'vinyl-buffer';
 import vinylSourceStream from 'vinyl-source-stream';
 import babelify from 'babelify';
+import rename from 'gulp-rename';
 
-const compileMainMinScripts = () => {
-  return browserify('source/js/main.js', { debug: true })
-    .transform(babelify, { presets: ['@babel/preset-env'] })
-    .bundle()
-    .pipe(vinylSourceStream('main.js'))
-    .pipe(vinylBuffer())
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest('build/js'));
-};
-
-const compileMainScripts = () => {
-  return browserify('source/js/main.js', { debug: true })
+// Функция для минификации скриптов
+const compileMainMinScripts = () =>
+  browserify('source/js/main.js', { debug: true })
     .transform(babelify, { presets: ['@babel/preset-env'] })
     .bundle()
     .pipe(vinylSourceStream('main.js'))
     .pipe(vinylBuffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(rename({ basename: 'main', extname: '.min.js' }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/js'));
-};
+
+const compileMainScripts = () =>
+  browserify('source/js/main.js', { debug: true })
+    .transform(babelify, { presets: ['@babel/preset-env'] })
+    .bundle()
+    .pipe(vinylSourceStream('main.js'))
+    .pipe(vinylBuffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(rename({ basename: 'main', extname: '.js' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/js'));
 
 export { compileMainMinScripts, compileMainScripts };
